0665ca27d2b99fcea0bd4ccc65ff124440dd2d1b
mechurk <londynska56>
2020-01-23 15:42:48 +0100

    definition function cf2

--- optimization_article.py
+++ optimization_article.py
@@ -3,13 +3,13 @@ import pulp as p
 Lp_prob = p.LpProblem('Problem', p.LpMinimize)
 
 building_ids = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']  # v
-center_ids = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']  # u
+center_ids = building_ids  # u
 heights = {'A': 10, 'B': 10, "C": 17, 'D': 17, 'E': 10, 'F': 10, 'G': 17, 'H': 17}
 footprints = {'A': 1, 'B': 1, "C": 1, 'D': 1, 'E': 1, 'F': 1, 'G': 1, 'H': 1}
 M = {'A': 10000, 'B': 10000, "C": 10000, 'D': 10000, 'E': 10000, 'F': 10000, 'G': 10000, 'H': 10000, }
 edges = [('A', 'B'), ('B', 'A'), ('B', 'C'), ('C', 'B'), ('C', 'D'), ('D', 'C'), ('E', 'F'), ('F', 'E'), ('F', 'G'),
          ('G', 'F'), ('G', 'H'), ('H', 'G')]
-building_count=len(building_ids)
+building_count = len(building_ids)
 # Xuv
 center_matrix = p.LpVariable.dicts("center_matrix", ((i, j) for i in building_ids for j in center_ids), lowBound=0,
                                    upBound=1, cat='Binary')
@@ -19,13 +19,14 @@ delta_volumes_matrix = p.LpVariable.dicts("delta_volume_matrix", ((i, j) for i i
 # Hu
 height_center = p.LpVariable.dicts("height_center", ((j) for j in center_ids), lowBound=0)
 
-#fa
-flows=p.LpVariable.dicts("flows", ((j) for j in edges))
+# fa
+flows = p.LpVariable.dicts("flows", ((j) for j in edges))
 
-#Fa
-positive_flows=p.LpVariable.dicts("positive_flows", ((j) for j in edges), lowBound=0,upBound=1,cat='Binary')
+# Fa
+positive_flows = p.LpVariable.dicts("positive_flows", ((j) for j in edges), lowBound=0, upBound=1, cat='Binary')
 print(flows)
-print (positive_flows)
+print(positive_flows)
+
 
 def cb1_one_building_id_all_center_ids(Lp_prob, building_id, center_ids):
     Lp_prob += p.lpSum(center_matrix[building_id, center_id] for center_id in center_ids) == 1
@@ -72,15 +73,29 @@ def printProb(Lp_prob):
         print(v.name, "=", v.varValue)
     print("Status:", p.LpStatus[Lp_prob.status])
 
-def cf1(Lp_prob,edges,flows,positive_flows):
+
+def cf1(Lp_prob, edges, flows, positive_flows):
     for edge in edges:
-        Lp_prob+=building_count*positive_flows[edge]>=flows[edge]
+        Lp_prob += building_count * positive_flows[edge] >= flows[edge]
+
+
+def cf2(Lp_prob, flows, building_ids):
+    for building_id in building_ids:
+        outcoming_edges = [edge for edge in edges if edge[0] == building_id]
+        incoming_edges = [edge for edge in edges if edge[1] == building_id]
+        Lp_prob += p.lpSum(flows[edge] for edge in outcoming_edges) - p.lpSum(
+            flows[edge] for edge in incoming_edges) >= 1 - center_matrix[building_id, building_id] * (
+                           building_count + 1)
+        Lp_prob += p.lpSum(flows[edge] for edge in outcoming_edges) - p.lpSum(
+            flows[edge] for edge in incoming_edges) <= 1 - center_matrix[building_id, building_id]
+
 
 cb1(Lp_prob, building_ids, center_ids)
 cb2(Lp_prob, building_ids, center_ids)
 c_delta_V(Lp_prob, building_ids, center_ids)
 objective_function(Lp_prob, building_ids, center_ids)
-cf1(Lp_prob,edges,flows,positive_flows)
+cf1(Lp_prob, edges, flows, positive_flows)
+cf2(Lp_prob, flows, building_ids)
 # result
 Lp_prob.solve()
 print(Lp_prob)
