27cf93d924b31b8ed70558e1f47fe5a3659ddc0b
mechurk <londynska56>
2020-01-24 18:28:53 +0100

    best best best function version, changed building id and center id. First function version.

--- optimization_article.py
+++ optimization_article.py
@@ -4,14 +4,14 @@ Lp_prob = p.LpProblem('Problem', p.LpMinimize)
 
 building_ids = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']  # v
 center_ids = building_ids  # u
-heights = {'A': 8, 'B': 8, "C": 16, 'D': 15, 'E': 1, 'F': 1, 'G': 7, 'H': 14}
+heights = {'A': 8, 'B': 8, "C": 15, 'D': 15, 'E': 1, 'F': 1, 'G': 8, 'H': 17}
 footprints = {'A': 10, 'B': 10, "C": 10, 'D': 10, 'E': 10, 'F': 10, 'G': 10, 'H': 10}
 M = {'A': 10000, 'B': 10000, "C": 10000, 'D': 10000, 'E': 10000, 'F': 10000, 'G': 10000, 'H': 10000}
 edges = [('A', 'B'), ('B', 'A'), ('B', 'D'), ('D', 'B'), ('B', 'C'), ('C', 'B'), ('B', 'E'), ('E', 'B'), ('E', 'F'),
-         ('F', 'E'), ('G', 'H'), ('H', 'G'),('D','E'),('E','D'),('E','C'),('C','E'),('C','F'),('F','C'),('F','G'),('G','F')]
+         ('F', 'E'), ('G', 'H'), ('H', 'G'), ('D', 'E'), ('E', 'D'), ('E', 'C'), ('C', 'E'), ('C', 'F'), ('F', 'C')]
 
-volume_change_weight = 1
-building_count = len(building_ids)-1
+volume_change_weight = 0
+building_count = len(building_ids)
 # Xuv
 center_matrix = p.LpVariable.dicts("center_matrix", ((i, j) for i in building_ids for j in center_ids), lowBound=0,
                                    upBound=1, cat='Binary')
@@ -22,7 +22,7 @@ delta_volumes_matrix = p.LpVariable.dicts("delta_volume_matrix", ((i, j) for i i
 height_center = p.LpVariable.dicts("height_center", ((j) for j in center_ids), lowBound=0)
 
 # fa
-flows = p.LpVariable.dicts("flows", ((j) for j in edges))
+flows = p.LpVariable.dicts("flows", ((j) for j in edges), lowBound=0)
 
 # Fa
 positive_flows = p.LpVariable.dicts("positive_flows", ((j) for j in edges), lowBound=0, upBound=1, cat='Binary')
@@ -31,7 +31,7 @@ print(positive_flows)
 
 
 def cb1_one_building_id_all_center_ids(Lp_prob, building_id, center_ids):
-    Lp_prob += p.lpSum(center_matrix[building_id, center_id] for center_id in center_ids) == 1
+    Lp_prob += p.lpSum(center_matrix[center_id, building_id] for center_id in center_ids) == 1
 
 
 def cb1(Lp_prob, building_ids, center_ids):
@@ -40,7 +40,7 @@ def cb1(Lp_prob, building_ids, center_ids):
 
 
 def cb2_two_building_ids_one_center_id(Lp_prob, first_building_id, second_building_id, center_id):
-    Lp_prob += center_matrix[first_building_id, center_id] <= center_matrix[second_building_id, center_id]
+    Lp_prob += center_matrix[center_id, first_building_id] <= center_matrix[center_id, second_building_id]
 
 
 def cb2(Lp_prob, building_ids, center_ids):
@@ -50,12 +50,12 @@ def cb2(Lp_prob, building_ids, center_ids):
 
 
 def c_delta_V_one_building_one_center(Lp_prob, building_id, center_id):
-    Lp_prob += delta_volumes_matrix[building_id, center_id] >= (heights[building_id] - height_center[center_id]) * \
+    Lp_prob += delta_volumes_matrix[center_id, building_id,] >= (heights[building_id] - height_center[center_id]) * \
                footprints[building_id] - (
-                       1 - center_matrix[building_id, center_id]) * M[building_id]
-    Lp_prob += delta_volumes_matrix[building_id, center_id] >= -(heights[building_id] - height_center[center_id]) * \
+                       1 - center_matrix[center_id, building_id,]) * M[building_id]
+    Lp_prob += delta_volumes_matrix[center_id, building_id,] >= -(heights[building_id] - height_center[center_id]) * \
                footprints[building_id] - (
-                       1 - center_matrix[building_id, center_id]) * M[building_id]
+                       1 - center_matrix[center_id, building_id]) * M[building_id]
 
 
 def c_delta_V(Lp_prob, building_ids, center_ids):
@@ -66,7 +66,7 @@ def c_delta_V(Lp_prob, building_ids, center_ids):
 
 def objective_function(Lp_prob, building_ids, center_ids):
     Lp_prob += p.lpSum(
-        center_matrix[building_ids[index], center_ids[index]] for index in
+        center_matrix[center_ids[index], building_ids[index]] for index in
         range(len(building_ids))) + (volume_change_weight * p.lpSum(
         delta_volumes_matrix))
 
@@ -115,7 +115,7 @@ cf1(Lp_prob, edges, flows, positive_flows)
 cf2(Lp_prob, flows, building_ids)
 cf3(Lp_prob, edges, building_ids, positive_flows)
 cf4(Lp_prob, building_ids, edges, positive_flows)
-
+# ch2(Lp_prob, building_ids, center_ids)
 
 # result
 Lp_prob.solve()
