3404848d575dcb49006c03db310840a371b9c256
mechurk <londynska56>
2020-01-25 20:12:55 +0100

    add parameter roof_volume funkcni

--- optimization_article.py
+++ optimization_article.py
@@ -4,16 +4,19 @@ Lp_prob = p.LpProblem('Problem', p.LpMinimize)
 
 building_ids = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']  # v
 center_ids = building_ids  # u
-heights = {'A': 10, 'B': 10, "C": 8, 'D': 8, 'E': 8, 'F': 8, 'G': 15, 'H': 8}
+heights = {'A': 10, 'B': 10, "C": 10, 'D':10, 'E':10, 'F': 10, 'G': 10, 'H': 10}
 footprints = {'A': 10, 'B': 10, "C": 10, 'D': 10, 'E': 10, 'F': 10, 'G': 10, 'H': 10}
 M = {'A': 10000, 'B': 10000, "C": 10000, 'D': 10000, 'E': 10000, 'F': 10000, 'G': 10000, 'H': 10000}
 edges = [('A', 'B'), ('B', 'A'), ('B', 'C'), ('C', 'B'), ('C', 'D'), ('D', 'C'), ('D', 'E'), ('E', 'D'), ('E', 'F'),
          ('F', 'E'), ('F', 'G'), ('G', 'F')]
 
-roof_types = {'A': 1, 'B': 1, "C": 2.5, 'D': 3, 'E': 8, 'F': 8, 'G': 8, 'H': 3}
+roof_types = {'A': 1, 'B': 1, "C": 2.5, 'D': 3, 'E': 2.7, 'F': 8, 'G': 8, 'H': 3}
+roof_volumes= {'A': 10, 'B': 11, "C": 8, 'D':50, 'E': 20, 'F': 8, 'G': 17, 'H':4}
 volume_change_weight = 0.1
 building_count = len(building_ids)
 epsilon_roof_type = 0.6
+epsilon_roof_volume = 5
+
 
 # Xuv
 center_matrix = p.LpVariable.dicts("center_matrix", ((i, j) for i in building_ids for j in center_ids), lowBound=0,
@@ -132,6 +135,14 @@ def rooftypes(Lp_prob, center_ids, buiding_ids, roof_types):
             Lp_prob += center_matrix[center_id, building_id] * (
                         roof_types[center_id] - roof_types[building_id]) >= -epsilon_roof_type
 
+def roofvolumes(Lp_prob, center_ids, buiding_ids, roof_volumes):
+    for center_id in center_ids:
+        for building_id in building_ids:
+            Lp_prob += center_matrix[center_id, building_id] * (
+                        roof_volumes[center_id] - roof_volumes[building_id]) <= epsilon_roof_volume
+            Lp_prob += center_matrix[center_id, building_id] * (
+                        roof_volumes[center_id] - roof_volumes[building_id]) >= -epsilon_roof_volume
+
 
 cb1(Lp_prob, building_ids, center_ids)
 cb2(Lp_prob, building_ids, center_ids)
@@ -144,7 +155,7 @@ cf4(Lp_prob, building_ids, edges, positive_flows)
 # ch2(Lp_prob, center_ids, center_ids)
 # ch3(Lp_prob, center_ids, center_ids)
 rooftypes(Lp_prob, center_ids, building_ids, roof_types)
-
+roofvolumes(Lp_prob, center_ids, building_ids, roof_volumes)
 # result
 Lp_prob.solve()
 print(Lp_prob)
