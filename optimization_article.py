ea4d155916caff9520146c9b90cb738fe2240987
mechurk <londynska56>
2020-01-31 22:14:16 +0100

    add funciton of calculating M_vol_volume

--- optimization_article.py
+++ optimization_article.py
@@ -1,21 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import pulp as p
+import networkx as nx
 
 Lp_prob = p.LpProblem('Problem', p.LpMinimize)
 
 building_ids = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']  # v
 center_ids = building_ids  # u
-heights = {'A': 10, 'B': 10, "C": 10, 'D':10, 'E':10, 'F': 10, 'G': 10, 'H': 10} #h
-footprints = {'A': 10, 'B': 10, "C": 10, 'D': 10, 'E': 10, 'F': 10, 'G': 10, 'H': 10} #A
-M = {'A': 10000, 'B': 10000, "C": 10000, 'D': 10000, 'E': 10000, 'F': 10000, 'G': 10000, 'H': 10000} #Mvolume
+heights = {'A': 10, 'B': 10, "C": 8, 'D':5, 'E':5, 'F': 10, 'G': 55, 'H': 57} #h
+footprints = {'A': 10, 'B': 10, "C": 10, 'D': 50, 'E': 55, 'F': 10, 'G': 10, 'H': 10} #A
+#M = {'A': 10000, 'B': 10000, "C": 10000, 'D': 10000, 'E': 10000, 'F': 10000, 'G': 10000, 'H': 10000} #Mvolume
 edges = [('A', 'B'), ('B', 'A'), ('B', 'C'), ('C', 'B'), ('C', 'D'), ('D', 'C'), ('D', 'E'), ('E', 'D'), ('E', 'F'),
-         ('F', 'E'), ('F', 'G'), ('G', 'F')] #edges
+         ('F', 'E'), ('F', 'G'), ('G', 'F'),('G','H'),('H','G')] #edges
 
-roof_types = {'A': 1, 'B': 1, "C": 2.5, 'D': 3, 'E': 2.7, 'F': 8, 'G': 8, 'H': 3} #R
-roof_volumes= {'A': 10, 'B': 11, "C": 8, 'D':50, 'E': 20, 'F': 8, 'G': 17, 'H':4} #Vroof
-volume_change_weight = 0.1 # W objective function
+roof_types = {'A': 1, 'B': 1, "C": 1, 'D': 1, 'E': 1, 'F': 1, 'G': 1, 'H': 1} #R
+roof_volumes= {'A': 1, 'B': 1, "C": 1, 'D':1, 'E': 1, 'F': 1, 'G': 1, 'H':1} #Vroof
+volume_change_weight = 0.01 # W objective function
 building_count = len(building_ids) #M cf1, cf2
-epsilon_roof_type = 0.6
-epsilon_roof_volume = 5
+epsilon_roof_type = 1
+epsilon_roof_volume = 1
 
 
 # Xuv
@@ -113,7 +116,7 @@ def cf4(Lp_prob, building_ids, edges, positive_flows):
             positive_flows[edge] for edge in outcoming_edges) <= 1
 
 
-"""
+
 def ch2(Lp_prob, center_ids, building_ids):
     for center_id in center_ids:
         for building_id in building_ids:
@@ -124,7 +127,7 @@ def ch3(Lp_prob, center_ids, building_ids):
     for center_id in center_ids:
         for building_id in building_ids:
             Lp_prob += center_matrix[center_id, building_id] * (heights[building_id] - height_center[center_id]) >= 0
-"""
+
 
 
 def rooftypes(Lp_prob, center_ids, buiding_ids, roof_types):
@@ -143,7 +146,61 @@ def roofvolumes(Lp_prob, center_ids, buiding_ids, roof_volumes):
             Lp_prob += center_matrix[center_id, building_id] * (
                         roof_volumes[center_id] - roof_volumes[building_id]) >= -epsilon_roof_volume
 
+def calculate_M_vo_volume(bld_nb, height, footprints):
+    # create graf and calculate connected components and create lis of these components
+    G = nx.Graph()
+    for i in bld_nb:
+        G.add_node(i[0])
 
+        bld = i[0]
+
+        for a in i:
+            if a != bld and G.has_edge(bld, a) == 0:
+                # print (a)
+                G.add_edge(bld, a)
+
+    n = nx.number_connected_components(G)
+    con = nx.connected_components(G)
+    cc = list(con)
+
+    # dict
+    M = {}
+    M_all = {}
+
+    # find maximum and minimum hight value in component
+    for block in cc:
+        heights_block = [0]
+        for building in block:
+            for h in height:
+                if h == building:
+                    heights_block.append(height[h])
+        max_block = max(heights_block)
+        min_block = min(heights_block)
+        #print(heights_block)
+
+        # calculate max (height[h]-min_block,max_block-height[h]) and add to dict M
+        for building in block:
+            h_set = []
+            for h in height:
+                if h == building:
+                    minimum = height[h] - min_block
+                    maximum = max_block - height[h]
+                    h_set.append(minimum)
+                    h_set.append(maximum)
+                    total_h = max(h_set)
+                    M[h] = total_h
+
+    # print (M)
+    # multiply M value with footprint
+    for par in M:
+        for footprint in footprints:
+            if par == footprint:
+                M_final = M[par] * footprints[footprint]
+                M_all[footprint] = M_final
+
+    return (M_all)
+
+M=calculate_M_vo_volume(edges, heights, footprints)
 cb1(Lp_prob, building_ids, center_ids)
 cb2(Lp_prob, building_ids, center_ids)
 c_delta_V(Lp_prob, building_ids, center_ids)
@@ -152,8 +209,8 @@ cf1(Lp_prob, edges, flows, positive_flows)
 cf2(Lp_prob, flows, building_ids)
 cf3(Lp_prob, edges, building_ids, positive_flows)
 cf4(Lp_prob, building_ids, edges, positive_flows)
-# ch2(Lp_prob, center_ids, center_ids)
-# ch3(Lp_prob, center_ids, center_ids)
+#ch2(Lp_prob, center_ids, center_ids)
+#ch3(Lp_prob, center_ids, center_ids)
 rooftypes(Lp_prob, center_ids, building_ids, roof_types)
 roofvolumes(Lp_prob, center_ids, building_ids, roof_volumes)
 # result
